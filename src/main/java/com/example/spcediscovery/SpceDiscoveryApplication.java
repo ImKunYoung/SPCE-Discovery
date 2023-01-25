f8a6951d5a0f0e8e51680f17ae66fd87a4f86c07
ImKunYoung <ur2kunyoung2@outlook.com>
2023-01-25 13:15:15 +0900

    initial commit

---
+++ src/main/java/com/example/spcediscovery/SpceDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.example.spcediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpceDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpceDiscoveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/spcediscovery/SpceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spcediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
